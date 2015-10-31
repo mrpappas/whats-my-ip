f25a0fa0eebf5f38696e3ac8579ccee989d67de3
Ben.Pappas <mrbenpappas@gmail.com>
2015-10-31 11:55:46 -0400

    formatting index.php

--- index.html.php
+++ index.html.php
@@ -33,9 +33,13 @@
 			  <div class="panel-body">
 				<!---$ip goes here--->
 				<h1><?php echo $ip?></h1>
+				<!---address goes here--->
 				<h4 id="address"></h2>
+				<!--- Country of origin go here --->
 				<h5 id="details"></h5>
+				<!--- Longitude and Lattitude go here --->
 				<h5 id="geo"></h5>
+				<!--- isp info goes here --->
 				<h5 id="isp"></h5>
 			  </div>
 			</div>
@@ -44,13 +48,17 @@
 		
 		<div class="row" align="left">
 		  <div class="col-md-6">
-			<h4><strong style="color:red">Note:</strong> if you are using a proxy or VPN this will not be accurate.
+			<h4><strong style="color:red">Note:</strong> if you are using a proxy or VPN this will not be accurate.</h4>
+			<h4><i class="fa fa-globe"></i> Geolocational data is an estimate derived from <a href="http://ipinfo.io/" target="_blank">ipinfo.io</a>, it is not an exact location.</h4>
+			
 		  </div>
 	    </div>
 	</div>
 	
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+  
+  <!-- http://info.io for locational data , could get the ip address from here as well but i did not know how to grab an ip address in PHP so i did in this project-->
   
   <script>
   
